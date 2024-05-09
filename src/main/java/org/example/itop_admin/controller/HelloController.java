f23e91ad406a12ea62b0661b302b25a1aad5179e
zhaoyb <593163583@qq.com>
2024-05-09 11:11:49 +0800

    修正目录结构

--- src/main/java/org/example/itop_admin/controller/HelloController.java
+++ src/main/java/org/example/itop_admin/controller/HelloController.java
@@ -1,4 +1,4 @@
-package org.example.itop_admin.config.controller;
+package org.example.itop_admin.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;

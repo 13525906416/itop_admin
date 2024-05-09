31741f4d2f2f55cec1198d4d88e5115347a62344
zhaoyb <593163583@qq.com>
2024-05-09 09:06:19 +0800

    初始化项目 springsecurity-v2 根据内置H2数据库存储用户

--- src/main/java/org/example/itop_admin/config/SecurityConfig.java
+++ src/main/java/org/example/itop_admin/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package org.example.itop_admin.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,13 +13,18 @@ import org.springframework.security.config.annotation.web.configurers.oauth2.ser
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
 import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 /**
  * @ClassName SecurityConfig  5.4以后的版本都是通过组件的方式进行配置，不需要继承WebSecurityConfigurerAdapter，重写里面方法实现
@@ -29,58 +36,55 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-
-
-//    5.4版本以后基于内存的管理器
+    //使用内嵌的H2数据库，用框架提供的ddl脚本创建
     @Bean
-    public UserDetailsManager users() {
-        return new InMemoryUserDetailsManager(
-                User.withUsername("user")
-                        .password("user")
-                        .authorities("app")
-                        .roles("USER")
-                        .build(),
-                User.withUsername("admin")
-                        .password("admin")
-                        .roles("ADMIN")
-                        .build()
-        );
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+
+    //5.4版本以后基于jdbc方式实现
+    @Bean
+    public UserDetailsManager users(DataSource dataSource) {
+        UserDetails user = User.withUsername("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        users.createUser(user);
+        users.createUser(admin);
+        return users;
     }
 
     @Bean
-    public PasswordEncoder getPasswordEncoder(){
+    public PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/admin")
+                                .requestMatchers("/admin")
 //                        .hasAnyRole("USER","ADMIN")
                                 .hasRole("ADMIN")
                                 .requestMatchers("user")
-                                .hasAnyRole("USER","ADMIN")
+                                .hasAnyRole("USER", "ADMIN")
                                 .requestMatchers("/")
                                 .permitAll()
                 )
                 .formLogin(formLogin -> formLogin
 //                        .loginPage("/login")
-                        .permitAll()
+                                .permitAll()
 
                 );
         return http.build();
     }
 }
-
-//        http
-//                .authorizeHttpRequests((authorize) -> authorize
-//                .anyRequest().authenticated()
-//                )
-//                .csrf((csrf) -> csrf.ignoringRequestMatchers("/token"))
-//                .httpBasic(Customizer.withDefaults())
-//                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
-//                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .exceptionHandling((exceptions) -> exceptions
-//                .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
-//                .accessDeniedHandler(new BearerTokenAccessDeniedHandler())
-//                );

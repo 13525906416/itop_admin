85c5c452e85fd767badddea1f07f03dca2b521c7
zhaoyb <593163583@qq.com>
2024-05-16 09:36:38 +0800

    oauth2 授权码模式 基于内存实现

---
+++ authorization-server/src/main/java/org/example/itop_admin/AuthorizationApplication.java
@@ -0,0 +1,18 @@
+package org.example.itop_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName org.example.itop_admin.Application
+ * @Author zhaoyb
+ * @Date 2024-05-14
+ * @Version 1.0
+ */
+
+@SpringBootApplication
+public class AuthorizationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationApplication.class,args);
+    }
+}
